2cb976a2eb98a7424420ed56ba4a23e0d917624b
AugusDogus <9794679+AugusDogus@users.noreply.github.com>
2024-11-03 22:28:12 -0600

    fix: patching

--- src/patches/message.tsx
+++ src/patches/message.tsx
@@ -13,20 +13,29 @@ export function patchMessages() {
             return;
         }
 
-        messagesPatch = before("default", Text, (props) => {
-            try {
-                // Log all text content to help us identify username texts
-                if (props[0]?.children) {
-                    logger.log("Text content:", props[0].children);
-                    logger.log("Text props:", props[0]);
-                }
-            } catch (err) {
-                logger.log("Error in patch callback:", err);
-            }
-        });
+        // Log what we found to help debug
+        logger.log("Found Text component:", Text);
 
-        logger.log("Successfully patched Text component");
+        // Try different ways to patch the component
+        if (Text.render) {
+            messagesPatch = before("render", Text, (props) => {
+                logger.log("Text render props:", props[0]);
+            });
+        } else if (typeof Text === 'function') {
+            messagesPatch = before(null, Text, (props) => {
+                logger.log("Text function props:", props[0]);
+            });
+        } else if (Text.default?.render) {
+            messagesPatch = before("render", Text.default, (props) => {
+                logger.log("Text default.render props:", props[0]);
+            });
+        } else {
+            logger.log("Could not find patchable function on Text component:", Object.keys(Text));
+        }
+
+        logger.log("Successfully set up patch");
     } catch (err) {
-        logger.log("Error while setting up patch:", err);
+        logger.log("Error while setting up patch:", err.message);
+        logger.log(err.stack);
     }
 } 
